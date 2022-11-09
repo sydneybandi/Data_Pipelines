5cdc18b78225da5c656e16e05fe72fe1add4e9c0
Sydney Bandi <45433877+sydneybandi@users.noreply.github.com>
2022-11-09 19:58:28 +0300

    Uploading the first files

---
+++ prefect-docker/src/create_report.py
@@ -0,0 +1,31 @@
+import datapane as dp
+from prefect import flow, task
+from prefect_shell import shell_run_command
+from prefect.blocks.system import Secret
+
+
+@task
+def get_dp_token():
+    secret_block = Secret.load("eb1ab893d35606be17134933ea82d72d74601312")
+
+    # Access the stored secret
+    return secret_block.get()
+
+
+@flow
+def login_into_datapane():
+    token = get_dp_token()
+    return shell_run_command(f"datapane login --token {token}")
+
+
+@task
+def upload_report(report_elements: list, keyword: str):
+    dp.Report(*report_elements).upload(
+        name=f"{keyword.title()} Report", publicly_visible=True
+    )
+
+
+@flow(name="Create a Report")
+def create_report(report_elements: list, keyword: str):
+    login_into_datapane()
+    upload_report(report_elements, keyword)
